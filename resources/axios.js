d0061ac61045879cdf1c46a26b87142a112e8590
Andrew Cravenho <andrew@cravenho.me>
2019-04-17 20:12:39 -0400

    Add post to axios module

--- resources/axios.js
+++ resources/axios.js
@@ -11,5 +11,16 @@ module.exports = {
       }
     })
     return results
+  },
+  postData: async (url, data) => {
+    let results = await new Promise(async (resolve, reject) => {
+      try {
+        let res = await axios.post(url, data)
+        resolve(res.data)
+      } catch (error) {
+        reject(error)
+      }
+    })
+    return results
   }
 }
