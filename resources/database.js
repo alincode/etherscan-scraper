c5e08a021c201b126fb8aa7909d8396b763b3b9f
Andrew Cravenho <andrew@cravenho.me>
2019-04-22 19:59:59 -0400

    Use cloud MYSQL DB

--- resources/database.js
+++ resources/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql')
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
+  host: 'etherscan.cdl6z46qyrxn.us-east-1.rds.amazonaws.com',
+  user: 'blockscout',
+  password: 'Abelance9',
   database: 'etherscan_contracts'
 })
 
